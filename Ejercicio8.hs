9e672cbffa8bb7665004ef32f466f51535fceb6d
IvanR-6257 <ivanr6257@gmail.com>
2024-04-19 19:52:48 -0300

    feature signup working

--- Ejercicio8.hs
+++ Ejercicio8.hs
@@ -1,8 +1,7 @@
-iesimoDigito :: Integer −> Integer −> Integer
-iesimoDigito n i | i == cantDigitos n = digitoUnidades n
- otherwise = 1 + iesimoDigito (sacarUnidades n ) i
-where sacarUnidades n = div n 10
-digitoUnidades n = mod n 10
+iesimoDigito :: Int -> Int -> Int
+iesimoDigito n i = mod (div n (10^(cantDigitos n - i))) 10
+
 cantDigitos :: Int -> Int
-cantDigitos n | n < 10 = 1
-              | otherwise = 1 + cantDigitos (sacarUnidades n) where sacarUnidades n = div n 10
+cantDigitos j
+      | j == 0 = 0
+      | otherwise = 1 + cantDigitos (div j 10)
